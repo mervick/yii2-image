9c8374e777a76b0cbec6a2a98277580c13fd9d26
Andrey Izman <izman.dev@gmail.com>
2015-06-22 15:54:46 +0300

    Implements drivers/GD

--- drivers/GD.php
+++ drivers/GD.php
@@ -1,3 +1,117 @@
 <?php
 
 namespace mervick\image\drivers;
+
+use mervick\image\Driver;
+use mervick\image\DriverInterface;
+
+class GD extends Driver
+{
+    /**
+     * @var resource Image resource
+     */
+    protected $image;
+
+
+    /**
+     * Checks if GD extension is loaded.
+     * @return boolean
+     * @throws \ErrorException
+     */
+    public static function isLoaded()
+    {
+        if ((!extension_loaded('gd') && !extension_loaded('gd2')) || !function_exists('gd_info')) {
+            throw new \ErrorException('GD extension not loaded. Check your server configuration.');
+        }
+
+        if (!defined('GD_VERSION')) {
+            preg_match('/\d+\.\d+(?:\.\d+)?/', gd_info()['GD Version'], $m);
+            define('GD_VERSION', $m[0]);
+        }
+
+        if (version_compare(GD_VERSION, '2.0.1', '<')) {
+            throw new \ErrorException(sprintf('Image driver `GD` requires GD library version 2.0.1 or greater, you have %s', GD_VERSION));
+        }
+
+        return true;
+    }
+
+    /**
+     * Read image.
+     * @param string $filename
+     * @param boolean $throwsErrors
+     * @throws \InvalidParamException
+     */
+    public function __construct($filename, $throwsErrors = true)
+    {
+        static $isLoaded;
+        if (!isset($isLoaded)) {
+            $isLoaded = self::isLoaded();
+        }
+
+        parent::__construct($filename, $throwsErrors);
+
+        $create_func = 'createfrom' . $this->getFormat();
+        $this->image = $create_func($filename);
+    }
+
+    /**
+     * Get image format by extension or by $type
+     * @param null $extension
+     * @param null $quality
+     * @param null $type
+     * @return string
+     */
+    private function getFormat($extension = null, &$quality = null, &$type = null)
+    {
+        if (empty($extension)) {
+            $extension = image_type_to_extension($this->type, false);
+        }
+
+        $extension = strtolower($extension);
+
+        if (!in_array($extension, ['jpeg', 'gif', 'png'])) {
+            $extension = 'jpeg';
+        }
+
+        switch ($extension) {
+            case 'jpeg':
+                $format = 'jpeg';
+                $type = IMAGETYPE_JPEG;
+                $quality = $quality >= 0 ? min($quality, 100) : 75;
+                break;
+            case 'gif':
+                $format = 'gif';
+                $type = IMAGETYPE_GIF;
+                $quality = null;
+                break;
+            case 'png':
+                $format = 'png';
+                $type = IMAGETYPE_PNG;
+                $quality = $quality >= 0 ? min($quality, 9) : 9;
+                break;
+        }
+
+        return $format;
+
+    }
+
+    /**
+     * Destructor.
+     */
+    public function __destruct()
+    {
+        if (is_resource($this->image)) {
+            imagedestroy($this->image);
+        }
+    }
+
+    /**
+     * Rotate the image using driver.
+     * @param integer $degrees
+     */
+    protected function _rotate($degrees)
+    {
+
+    }
+}

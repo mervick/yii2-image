ae28c05c838e9f81ba5a2015fb333f7590427504
Andrey Izman <izman.dev@gmail.com>
2015-06-22 20:08:08 +0300

    Added resize() method to Driver

--- Driver.php
+++ Driver.php
@@ -12,9 +12,6 @@ use yii\base\InvalidParamException;
  */
 abstract class Driver
 {
-
-    const HORIZONTAL = 'horizontal';
-    const VERTICAL = 'vertical';
     /**
      * @var string file path
      */
@@ -48,8 +45,8 @@ abstract class Driver
 
     /**
      * Constructor. Opens an image.
-     * @param string $filename
-     * @param boolean $throwsErrors
+     * @param string $filename File path
+     * @param boolean $throwsErrors [optional] If true will be throws exceptions
      * @throws \InvalidParamException
      */
     public function __construct($filename, $throwsErrors = true)
@@ -86,6 +83,124 @@ abstract class Driver
     }
 
     /**
+     * Resize the image to the given size.
+
+     * @param integer $width New width
+     * @param integer $height New height
+     * @param string $master [optional] Master dimension. Default is 'auto'
+     * @return Driver
+     */
+    public function resize($width, $height, $master = Image::AUTO)
+    {
+        if (empty($width) && empty($height)) {
+            return $this;
+        }
+
+        switch ($master) {
+            case Image::CROP:
+                if (empty($width) || empty($height)) {
+                    return $this->resize($width, $height, Image::AUTO);
+                }
+
+                $master = $this->width / $this->height > $width / $height ? Image::HEIGHT : Image::WIDTH;
+                $this->resize($width, $height, $master);
+
+                if ($this->width !== $width || $this->height !== $height) {
+                    $offset_x = round(($this->width - $width) / 2);
+                    $offset_y = round(($this->height - $height) / 2);
+                    $this->crop($width, $height, $offset_x, $offset_y);
+                }
+                return $this;
+                break;
+
+            case Image::ADAPT:
+                $width = max(round(!empty($width) ? $width : $this->width * $height / $this->height, 1));
+                $height = max(round(!empty($height) ? $height : $this->height * $width / $this->width, 1));
+                if ($width / $height !== $this->width / $this->height) {
+                    $bg_width = $this->width;
+                    $bg_height = $this->height;
+                    $offset_x = $offset_y = 0;
+
+                    if ($width / $height > $this->width / $this->height) {
+                        $bg_width = floor($this->height * $width / $height);
+                        $offset_x = abs(floor(($bg_width - $this->width) / 2));
+                    } else {
+                        $bg_height = floor($this->width * $height / $width);
+                        $offset_y = abs(floor(($bg_height - $this->height) / 2));
+                    }
+
+                    $this->_adapt($bg_width, $bg_height, $offset_x, $offset_y);
+                }
+                break;
+
+            case Image::INVERSE:
+                $master = ($this->width / $width) > ($this->height / $height) ? Image::HEIGHT : Image::WIDTH;
+                break;
+
+            case Image::PRECISE:
+                if ($width / $height > $this->width / $this->height) {
+                    $height = $this->height * $width / $this->width;
+                } else {
+                    $width = $this->width * $height / $this->height;
+                }
+                break;
+        }
+
+        switch ($master) {
+            case Image::WIDTH:
+                if (!empty($width)) {
+                    $height = null;
+                    $master = Image::AUTO;
+                }
+                break;
+
+            case Image::HEIGHT:
+                if (!empty($height)) {
+                    $width = null;
+                    $master = Image::AUTO;
+                }
+                break;
+        }
+
+        if (empty($width)) {
+            $master = Image::HEIGHT;
+        } elseif (empty($height)) {
+            $master = Image::WIDTH;
+        } elseif ($master === Image::AUTO) {
+            $master = ($this->width / $width) > ($this->height / $height) ? Image::WIDTH : Image::HEIGHT;
+        }
+
+        switch ($master) {
+            case Image::WIDTH:
+                $height = $this->height * $width / $this->width;
+                break;
+            case Image::HEIGHT:
+                $width = $this->width * $height / $this->height;
+                break;
+        }
+
+        $this->_resize(max(round($width), 1), max(round($height), 1));
+
+        return $this;
+    }
+
+    /**
+     * Resize the image.
+     * @param integer $width
+     * @param integer $height
+     */
+    abstract protected function _resize($width, $height);
+
+    /**
+     * Adapt the image.
+     * @param integer $width
+     * @param integer $height
+     * @param integer $offset_x
+     * @param integer $offset_y
+     */
+    abstract protected function _adapt($width, $height, $offset_x, $offset_y);
+
+    /**
      * Rotate the image.
      * @param integer $degrees
      * @return Driver
@@ -115,12 +230,12 @@ abstract class Driver
 
     /**
      * Flip the image along the horizontal or vertical axis.
-     * @param string $direction May be Driver::HORIZONTAL, Driver::VERTICAL
+     * @param string $direction May be Image::HORIZONTAL, Image::VERTICAL
      * @return Driver
      */
     final public function flip($direction)
     {
-        $this->_flip($direction === Driver::HORIZONTAL ? Driver::HORIZONTAL : Driver::VERTICAL);
+        $this->_flip($direction === Image::HORIZONTAL ? Image::HORIZONTAL : Image::VERTICAL);
         return $this;
     }
 

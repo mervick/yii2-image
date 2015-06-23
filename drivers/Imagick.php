69ab9f691f5ef44f60a155af375c05f95e54e475
Andrey Izman <izman.dev@gmail.com>
2015-06-23 11:37:21 +0300

    Added _crop(), _rotate(), _flip(), _sharpen() to drivers/Imagick.php

--- drivers/Imagick.php
+++ drivers/Imagick.php
@@ -99,4 +99,53 @@ class Imagick extends Driver
         $this->height = $image->getImageHeight();
     }
 
+    /**
+     * Crop the image.
+     * @param int $width
+     * @param int $height
+     * @param int $offset_x
+     * @param int $offset_y
+     */
+    protected function _crop($width, $height, $offset_x, $offset_y)
+    {
+        if ($this->im->cropImage($width, $height, $offset_x, $offset_y)) {
+            $this->width = $this->im->getImageWidth();
+            $this->height = $this->im->getImageHeight();
+            $this->im->setImagePage($this->width, $this->height, 0, 0);
+        }
+    }
+
+    /**
+     * Rotate the image.
+     * @param int $degrees
+     */
+    protected function _rotate($degrees)
+    {
+        if ($this->im->rotateImage(new \ImagickPixel('transparent'), $degrees)) {
+            $this->width = $this->im->getImageWidth();
+            $this->height = $this->im->getImageHeight();
+            $this->im->setImagePage($this->width, $this->height, 0, 0);
+        }
+    }
+
+    /**
+     * Flip the image.
+     * @param string $direction
+     */
+    protected function _flip($direction)
+    {
+        $func = $direction === Image::HORIZONTAL ? 'flopImage' : 'flipImage';
+        $this->im->$func();
+    }
+
+    /**
+     * Sharpe the image.
+     * @param integer $amount
+     */
+    protected function _sharpen($amount)
+    {
+        $amount = ($amount < 5) ? 5 : $amount;
+        $amount = ($amount * 3.0) / 100;
+        $this->im->sharpenImage(0, $amount);
+    }
 }

b92f8d1eb9de324a8a456f914141b79cd2ecf152
Andrey Izman <izman.dev@gmail.com>
2015-06-23 11:30:03 +0300

    Added _resize(), _adapt() methods to drivers/Imagick.php

--- drivers/Imagick.php
+++ drivers/Imagick.php
@@ -35,7 +35,7 @@ class Imagick extends Driver
     /**
      * Open the image.
      * @param string $filename
-     * @param bool $throwsErrors
+     * @param boolean $throwsErrors
      * @throws InvalidParamException
      * @throws \ErrorException
      */
@@ -55,6 +55,48 @@ class Imagick extends Driver
         if (!$this->im->getImageAlphaChannel()) {
             $this->im->setImageAlphaChannel(\Imagick::ALPHACHANNEL_SET);
         }
+    }
+
+    /**
+     * Destructor.
+     */
+    public function __destruct()
+    {
+        $this->im->clear();
+        $this->im->destroy();
+    }
+
+    /**
+     * Resize the image.
+     * @param integer $width
+     * @param integer $height
+     */
+    protected function _resize($width, $height)
+    {
+        if ($this->im->scaleImage($width, $height)) {
+            $this->width = $this->im->getImageWidth();
+            $this->height = $this->im->getImageHeight();
+        }
+    }
+
+
+    /**
+     * Adaptation the image.
+     * @param int $width
+     * @param int $height
+     * @param int $offset_x
+     * @param int $offset_y
+     */
+    protected function _adapt($width, $height, $offset_x, $offset_y)
+    {
+        $image = new \Imagick();
+        $image->newImage($width, $height, 'none');
+        $image->compositeImage($this->im, \Imagick::COMPOSITE_ADD, $offset_x, $offset_y);
+        $this->im->clear();
+        $this->im->destroy();
+        $this->im = $image;
+        $this->width = $image->getImageWidth();
+        $this->height = $image->getImageHeight();
     }
 
 }

1cca7af1a3e401fc2ed9ca14d12be8bad18e69fc
Andrey Izman <izman.dev@gmail.com>
2015-06-23 12:29:36 +0300

    Added _reflection(), _watermark(), _background() to drivers/Imagick.php

--- drivers/Imagick.php
+++ drivers/Imagick.php
@@ -139,7 +139,7 @@ class Imagick extends Driver
     }
 
     /**
-     * Sharpe the image.
+     * Sharp the image.
      * @param integer $amount
      */
     protected function _sharpen($amount)
@@ -147,5 +147,93 @@ class Imagick extends Driver
         $amount = ($amount < 5) ? 5 : $amount;
         $amount = ($amount * 3.0) / 100;
         $this->im->sharpenImage(0, $amount);
+    }
+
+    /**
+     * Reflection.
+     * @param integer $height
+     * @param integer $opacity
+     * @param boolean $fade_in
+     */
+    protected function _reflection($height, $opacity, $fade_in)
+    {
+        $reflection = $this->im->clone();
+        $reflection->flipImage();
+        $reflection->cropImage($this->width, $height, 0, 0);
+        $reflection->setImagePage($this->width, $height, 0, 0);
+        $direction = array('transparent', 'black');
+
+        if ($fade_in) {
+            $direction = array_reverse($direction);
+        }
+
+        $fade = new \Imagick;
+        $fade->newPseudoImage($reflection->getImageWidth(), $reflection->getImageHeight(),
+            vsprintf('gradient:%s-%s', $direction));
+
+        $reflection->compositeImage($fade, \Imagick::COMPOSITE_DSTOUT, 0, 0);
+        $reflection->evaluateImage(\Imagick::EVALUATE_MULTIPLY, $opacity / 100, \Imagick::CHANNEL_ALPHA);
+
+        $image = new \Imagick;
+        $image->newImage($this->width, $this->height + $height, new \ImagickPixel);
+
+        $image->setImageAlphaChannel(\Imagick::ALPHACHANNEL_SET);
+        $image->setColorspace($this->im->getColorspace());
+
+        if ($image->compositeImage($this->im, \Imagick::COMPOSITE_SRC, 0, 0) &&
+            $image->compositeImage($reflection, \Imagick::COMPOSITE_OVER, 0, $this->height)) {
+            $this->im = $image;
+            $this->width = $this->im->getImageWidth();
+            $this->height = $this->im->getImageHeight();
+        }
+    }
+
+    /**
+     * Watermark.
+     * @param Driver $image
+     * @param integer $offset_x
+     * @param integer $offset_y
+     * @param integer $opacity
+     */
+    protected function _watermark(Driver $image, $offset_x, $offset_y, $opacity)
+    {
+        $watermark = new \Imagick;
+        $watermark->readImageBlob($image->render(), $image->filename);
+
+        if ($watermark->getImageAlphaChannel() !== \Imagick::ALPHACHANNEL_ACTIVATE) {
+            $watermark->setImageAlphaChannel(\Imagick::ALPHACHANNEL_OPAQUE);
+        }
+
+        if ($opacity < 100) {
+            $watermark->evaluateImage(\Imagick::EVALUATE_MULTIPLY, $opacity / 100, \Imagick::CHANNEL_ALPHA);
+        }
+
+        $this->im->compositeImage($watermark, \Imagick::COMPOSITE_DISSOLVE, $offset_x, $offset_y);
+    }
+
+    /**
+     * Fill the image background.
+     * @param integer $r
+     * @param integer $g
+     * @param integer $b
+     * @param integer $opacity
+     */
+    protected function _background($r, $g, $b, $opacity)
+    {
+        $color = sprintf('rgb(%d, %d, %d)', $r, $g, $b);
+
+        $background = new \Imagick;
+        $background->newImage($this->width, $this->height, new \ImagickPixel($color));
+
+        if ( ! $background->getImageAlphaChannel()) {
+            $background->setImageAlphaChannel(\Imagick::ALPHACHANNEL_SET);
+        }
+        $background->setImageBackgroundColor(new \ImagickPixel('transparent'));
+        $background->evaluateImage(\Imagick::EVALUATE_MULTIPLY, $opacity / 100, \Imagick::CHANNEL_ALPHA);
+        $background->setColorspace($this->im->getColorspace());
+
+        if ($background->compositeImage($this->im, \Imagick::COMPOSITE_DISSOLVE, 0, 0)) {
+            $this->im = $background;
+        }
     }
 }
